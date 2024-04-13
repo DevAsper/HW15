ec2ad43bd2282c2dac7cf5a197252d5cc75686e8
DevAsper <asponomaryov@yandex.ru>
2024-04-13 21:52:37 +0300

    create test

---
+++ test.py
@@ -0,0 +1,53 @@
+import pygame
+import random
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игровой Тир")
+icon = pygame.image.load("img/shoot_game_logo.jpg")
+pygame.display.set_icon(icon)
+
+target_image = pygame.image.load("img/target.png")
+target_width = 80
+target_height = 80
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+# Добавляем скорость движения по осям
+target_speed_x = 2
+target_speed_y = 2
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+running = True
+while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+                target_speed_x = random.randint(1, 5)
+                target_speed_y = random.randint(1, 5)
+
+    target_x += target_speed_x
+    target_y += target_speed_y
+
+    if target_x <= 0 or target_x >= SCREEN_WIDTH - target_width:
+        target_speed_x = -target_speed_x
+    if target_y <= 0 or target_y >= SCREEN_HEIGHT - target_height:
+        target_speed_y = -target_speed_y
+
+    screen.blit(target_image, (target_x, target_y))
+
+    pygame.display.update()
+
+pygame.quit()
