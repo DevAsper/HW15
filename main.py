e8a8f226fc89d281bb2c68294acd095f005044a5
DevAsper <asponomaryov@yandex.ru>
2024-04-13 16:48:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,7 +8,20 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set.mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игровой Тир")
-icon = pygame.image.load("")
+icon = pygame.image.load("img/shoot_game_logo.jpg")
+pygame.display.set_icon(icon)
+
+target_image = pygame.image.load("img/")
+target_width = 50
+target_height = 50
+
+target_x = random.radint(0, SCREEN_WIDTH - target_width)
+target_y = random.radint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+
 
 running = True
 while running:
